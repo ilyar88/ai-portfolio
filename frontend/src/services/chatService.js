05e313278c1cc624c246684ee58964642e1fe0c5
ilyar88 <irahmilevich@gmail.com>
2025-06-15 04:52:25 +0300

    Add changes

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export class ChatService {
   static async handleStreamingResponse(response, callbacks) {
