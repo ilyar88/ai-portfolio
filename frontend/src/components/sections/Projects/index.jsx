dc7531d39cc6ff1b7ce1d1af05268ab760d59d2f
ilyar88 <irahmilevich@gmail.com>
2026-02-14 01:33:23 +0200

    Update index.jsx file.

--- frontend/src/components/sections/Projects/index.jsx
+++ frontend/src/components/sections/Projects/index.jsx
@@ -3,7 +3,7 @@ import { Github } from 'lucide-react'
 import { useGithubRepos } from '../../../hooks/useGithubRepos'
 
 export const ProjectsSection = () => {
-  const { repos, loading, error } = useGithubRepos('ilyar88', 3)
+  const { repos, loading, error } = useGithubRepos('ilyar88', 4)
 
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error loading projects</div>
