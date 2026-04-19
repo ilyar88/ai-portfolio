5a254a27c1c9f64a991ea7a435c6477da687a6d9
ilyar88 <irahmilevich@gmail.com>
2026-04-20 00:40:57 +0300

    Change the Blog title to "Budling AI agents for testing - new" .

--- frontend/src/components/layout/config/navigationConfig.js
+++ frontend/src/components/layout/config/navigationConfig.js
@@ -63,7 +63,7 @@ export const sections = {
   },
   blog: {
     icon: Blog,
-    title: 'Blog',
+    title: 'Budling AI agents for testing - new',
     path: '/blog',
     color: 'from-blue-400 to-indigo-500'
   }
