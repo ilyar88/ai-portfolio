4a9ce2805cf9b60d22b5df738bf64da774409aab
ilyar88 <irahmilevich@gmail.com>
2026-08-31 00:43:41 +0300

    Add changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { MainLayout } from './components/layout/MainLayout'
 import { HomeSection } from './components/sections/Home'
 import { AboutSection } from './components/sections/About'
 import { ProjectsSection } from './components/sections/Projects'
-import { BlogSection } from './components/sections/Blog'
+import { FilesSection } from './components/sections/Files'
 import { ConfigProvider } from './config/ConfigProvider'
 
 const App = () => {
@@ -15,8 +15,7 @@ const App = () => {
             <Route path="/" element={<HomeSection />} />
             <Route path="/about-me" element={<AboutSection />} />
             <Route path="/projects" element={<ProjectsSection />} />
-            <Route path="/blog" element={<BlogSection />} />
-            <Route path="/blog/:postId" element={<BlogSection />} />
+            <Route path="/files/*" element={<FilesSection />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </MainLayout>
