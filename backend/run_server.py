211372a95fcc9591e80ad31fa032821a22cc2e11
ilyar88 <irahmilevich@gmail.com>
2026-08-31 00:41:18 +0300

    Add FilesRouter.

--- backend/run_server.py
+++ backend/run_server.py
@@ -1,6 +1,7 @@
 import os
 from app import factory
 from app.controllers.chat_router import ChatRouter
+from app.controllers.files_router import FilesRouter
 from app.controllers.voice_router import VoiceRouter
 from app.logs.logger import get_logger
 from app.startup.documents.init_documents import init_documents
@@ -22,6 +23,7 @@ async def create_application():
     chat_service = factory.chat_service()
     router = ChatRouter(chat_service=chat_service)
     app.include_router(router=router.router)
+    app.include_router(router=FilesRouter().router)
     app.include_router(router=VoiceRouter().router)
 
     return app
